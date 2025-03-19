37033f95c0657f6b3201bdb98e0842ea083948d8
DorianLEQUERE <120470692+DorianLEQUERE@users.noreply.github.com>
2025-03-19 16:33:26 +0100

    Code de la connexion MQTT et LoRa Receiver
    Groupe de 4 :
    Dorian LE QUERE
    Amandine PAILLAT
    Mathurin BERDER
    Alexandre MERCERON

--- app_main.c
+++ app_main.c
@@ -13,6 +13,8 @@
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 
+#include "lora.h"
+
 #define WIFI_SSID "iPhone de Dorian"
 #define WIFI_PASS "dorian22"
 #define MQTT_BROKER_URI "mqtt://test.mosquitto.org"
@@ -74,8 +76,8 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     switch (event->event_id) {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "Connecté au broker MQTT !");
-        esp_mqtt_client_subscribe(client, "tp/alban", 0);
-        esp_mqtt_client_publish(client, "tp/alban", "Dorian et Amandine <3", 0, 1, 0);
+        esp_mqtt_client_subscribe(client, "SALAD", 0);
+        esp_mqtt_client_publish(client, "SALAD", "POTATOTO", 0, 1, 0);
         break;
     
     case MQTT_EVENT_DATA:
@@ -109,6 +111,60 @@ void mqtt_app_start(void)
     esp_mqtt_client_start(client);
 }
 
+void task_rx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	uint8_t buf[256]; // Maximum Payload size of SX1276/77/78/79 is 255
+	while(1) {
+		lora_receive(); // put into receive mode
+		if (lora_received()) {
+			int rxLen = lora_receive_packet(buf, sizeof(buf));
+			ESP_LOGI(pcTaskGetName(NULL), "%d byte packet received:[%.*s]", rxLen, rxLen, buf);
+		}
+		vTaskDelay(1); // Avoid WatchDog alerts
+	} // end while
+}
+
+void lora()
+{
+	if (lora_init() == 0) {
+		ESP_LOGE(pcTaskGetName(NULL), "Does not recognize the module");
+		while(1) {
+			vTaskDelay(1);
+		}
+	}
+
+
+
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 868MHz");
+	lora_set_frequency(868e6); // 868MHz
+
+	lora_enable_crc();
+
+	int cr = 1;
+	int bw = 7;
+	int sf = 7;
+
+	lora_set_coding_rate(cr);
+	//lora_set_coding_rate(CONFIG_CODING_RATE);
+	//cr = lora_get_coding_rate();
+	ESP_LOGI(pcTaskGetName(NULL), "coding_rate=%d", cr);
+
+	lora_set_bandwidth(bw);
+	//lora_set_bandwidth(CONFIG_BANDWIDTH);
+	//int bw = lora_get_bandwidth();
+	ESP_LOGI(pcTaskGetName(NULL), "bandwidth=%d", bw);
+
+	lora_set_spreading_factor(sf);
+	//lora_set_spreading_factor(CONFIG_SF_RATE);
+	//int sf = lora_get_spreading_factor();
+	ESP_LOGI(pcTaskGetName(NULL), "spreading_factor=%d", sf);
+
+	xTaskCreate(&task_rx, "RX", 1024*3, NULL, 5, NULL);
+
+}
+
+
 void app_main(void)
 {
     ESP_LOGI(TAG, "Démarrage...");
@@ -122,4 +178,6 @@ void app_main(void)
     ESP_LOGI(TAG, "Wi-Fi connecté, lancement de MQTT...");
 
     mqtt_app_start();
+
+    lora();
 }
